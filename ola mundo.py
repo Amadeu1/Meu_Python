6f0865754cc26dcb310f8b32e7fa6eabefdf92ad
Your Name <you@example.com>
2022-03-18 20:43:34 -0300

    salvando

---
+++ ola mundo.py
@@ -0,0 +1,14 @@
+
+salario = float(input("Qual seu salario:"))
+salario1 = salario*32/100
+salario2 = salario*10/100
+print(salario1)
+print(salario2)
+
+nome = input("qual seu nome:")
+nome = nome.upper()
+print(nome)
+
+ano = input("Qual ano você nasceu")
+idade = 2022 - int(ano)
+print(idade)
